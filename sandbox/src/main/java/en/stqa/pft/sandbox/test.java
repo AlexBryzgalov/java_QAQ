a180cf5830712b231bdd593a5282013af61ac12e
AlexBryzgalov <60262593+AlexBryzgalov@users.noreply.github.com>
2022-10-23 20:41:05 +0300

    indentation changed

--- sandbox/src/main/java/en/stqa/pft/sandbox/test.java
+++ sandbox/src/main/java/en/stqa/pft/sandbox/test.java
@@ -2,8 +2,8 @@ package en.stqa.pft.sandbox;
 
 public class test {
 
-    public static void main(String[] args) {
-        
-System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+
+    System.out.println("Hello world!");
+  }
 }
