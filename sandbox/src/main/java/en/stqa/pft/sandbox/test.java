b566ff574efcc93941bd1918b5f25c558bbcd243
AlexBryzgalov <60262593+AlexBryzgalov@users.noreply.github.com>
2022-10-25 11:43:06 +0300

    sqaure area form

--- sandbox/src/main/java/en/stqa/pft/sandbox/test.java
+++ sandbox/src/main/java/en/stqa/pft/sandbox/test.java
@@ -5,5 +5,8 @@ public class test {
   public static void main(String[] args) {
 
     System.out.println("Hello world!");
+    int side = 6;
+    System.out.println("Square area with side of " + side + " = " + (side * side));
+
   }
 }
