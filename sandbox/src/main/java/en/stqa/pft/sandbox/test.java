4eb560b0910ea79b1dbc0c5ea9b77d80d505e3d0
AlexBryzgalov <60262593+AlexBryzgalov@users.noreply.github.com>
2022-10-31 16:53:21 +0300

    functions_to_methods_jv

--- sandbox/src/main/java/en/stqa/pft/sandbox/test.java
+++ sandbox/src/main/java/en/stqa/pft/sandbox/test.java
@@ -3,10 +3,25 @@ package en.stqa.pft.sandbox;
 public class test {
 
   public static void main(String[] args) {
+    hello("world");
+    hello("Alex");
+    hello("user");
+    Square s = new Square(8);
 
-    System.out.println("Hello world!");
-    int side = 6;
-    System.out.println("Square area with side of " + side + " = " + (side * side));
 
+    System.out.println("Square area with the side " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(10, 4);
+
+    System.out.println("Rectangle area with the sides " + r.a + " and " + r.b + " = " + r.area());
   }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+
+
+
 }
+
